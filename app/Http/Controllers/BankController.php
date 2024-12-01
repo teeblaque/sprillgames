e6fe6f73e7cee37a4304df5162def756c5b1f326
Ajayi Taiwo <ajayitaiwo89@yahoo.com>
2024-12-01 07:19:29 +0100

    add bank and transaction history

---
+++ app/Http/Controllers/BankController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paystack;
+use App\Models\UserBank;
+use App\Models\UserCard;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class BankController extends Controller
+{
+    use ApiResponser;
+
+    public function store(Request $request) 
+    {
+        try {
+            $validator = Validator::make($request->input(), [
+                'bank_code' => 'required',
+                'account_number' => 'required',
+                'account_name' => 'required',
+                'bank_name' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return $this->error($validator->errors()->first(), 400);
+            }
+
+            DB::beginTransaction();
+            $paystack = new Paystack();
+
+            $bank = UserBank::create([
+                'user_id' => Auth::id(),
+                'provider' => 'commercial',
+                'bank_code' => $request->bank_code,
+                'account_name' => $request->account_name,
+                'account_number' => $request->account_number,
+                'bank_name' => $request->bank_name,
+                'loan_id' => 0
+            ]);
+            if ($bank) {
+                $tx = $paystack->createTransferRecipient($bank);
+            }
+            DB::commit();
+            return $this->success('Bank account saved successfully', $bank, 201);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return $this->error($th->getMessage(), 500);
+        }
+    }
+
+    public function index()
+    {
+        $banks = UserBank::where(['user_id' => Auth::id(), 'provider' => 'commercial'])->get();
+        return $this->success('Bank retrieved', $banks);
+    }
+    
+    public function removeBank($id)
+    {
+        try {
+            $bank = UserBank::where(['id' => $id, 'user_id' => Auth::id()])->first();
+            if ($bank && $bank->delete()) {
+                return $this->success('Bank Account removed successfully', [], 200);
+            }else{
+                return $this->error('Invalid bank Id', 400);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 500);
+        }
+    }
+
+    public function removeCard($id)
+    {
+        try {
+            $bank = UserCard::where(['id' => $id, 'user_id' => Auth::id()])->first();
+            if ($bank && $bank->delete()) {
+                return $this->success('User card removed successfully', [], 200);
+            }else{
+                return $this->error('Invalid bank Id', 400);
+            }
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage(), 500);
+        }
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\WalletTransaction;
 use App\Traits\ApiResponser;
 use App\Traits\HasPhoneFieldTrait;
 use Illuminate\Http\Request;
@@ -16,5 +17,38 @@ class UserController extends Controller
     {
         $user = User::where('id', Auth::id())->with(['wallet'])->first();
         return $this->success('Record found', $user);
+    }
+
+    public function remove()
+    {
+        $user = User::findOrFail(Auth::id());
+        if ($user->delete()) {
+            return $this->success('Account deleted successfully');
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user()->usermeta;
+        if (!$user->isemailVerified) {
+            $user->update([
+                'email' => $request->email
+            ]);
+    
+            if (isset($request->firstname) || isset($request->lastname)) {
+                $user->usermeta->update([
+                    'firstname' => $request->firstname,
+                    'lastname' => $request->lastname
+                ]);
+            }
+        }else{
+            return $this->error('You cannot update a verified mail', 400);
+        }
+    }
+
+    public function transactionHistory(Request $request)
+    {
+        $transactions = WalletTransaction::where('user_id', Auth::id())->latest()->paginate($request->limit ?? 20);
+        return $this->success('Record retrieved', $transactions);
     }
 }
