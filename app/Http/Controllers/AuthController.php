fdbcfc9275729d7aec55c25b52a7e5697a6418f5
Ajayi Taiwo <ajayitaiwo89@yahoo.com>
2025-03-09 15:43:18 +0100

    fixed stake amount bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        try {
+        // try {
             $validator = Validator::make($request->all(), [
                 'phone' => 'required|unique:users,phone',
                 'username' => 'required|unique:users,username',
@@ -72,9 +72,9 @@ class AuthController extends Controller
             $success['phone_code'] = $response;
 
             return $this->success('User Registration was successful.', $success, 200);
-        } catch (\Throwable $th) {
-            return $this->error($th->getMessage(), 500);
-        }
+        // } catch (\Throwable $th) {
+        //     return $this->error($th->getMessage(), 500);
+        // }
     }
 
     /**
