16fa527193df59263c895b77e033bc37fe8ff2e4
Ajayi Taiwo <ajayitaiwo89@yahoo.com>
2025-03-09 08:12:53 +0100

    disabled namecheap

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,11 +53,11 @@ class AuthController extends Controller
             ]);
             Wallet::create(['user_id' => $user->id]);
 
-            if (!Newsletter::isSubscribed($request->email)) {
-                Newsletter::subscribe($request->email, [
-                    'FNAME' => $request->name
-                ]);
-            }
+            // if (!Newsletter::isSubscribed($request->email)) {
+            //     Newsletter::subscribe($request->email, [
+            //         'FNAME' => $request->name
+            //     ]);
+            // }
 
             // VerifyAccount::dispatchAfterResponse($user, $otp);
             $params = [
