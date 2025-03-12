525c982a66f62936ee2fcad6e5af22268f49649e
Ajayi Taiwo <ajayitaiwo89@yahoo.com>
2025-03-12 23:37:38 +0100

    updated phone verification bug

--- app/Http/Controllers/AddFundController.php
+++ app/Http/Controllers/AddFundController.php
@@ -31,10 +31,8 @@ class AddFundController extends Controller
             $checkTransaction = Transaction::where('reference', $request->reference)->first();
             if(!$checkTransaction) return $this->error('Invalid payment reference', 400);
 
-
             $paystack = new Paystack;
             $tx = $paystack->verifyTransaction($request->reference);
-            dd($tx);
             if ($tx->status) {
                 $paymentData = $tx->data;
                 $paymentStatus = $paymentData->status;
